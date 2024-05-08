db1b9759a081714230bcc884e057491596a6ca70
Chrispine Oburu <chrizoburu65@gmail.com>
2024-05-08 19:19:04 +0300

    add delte and edit

--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -1,34 +1,161 @@
 // import React from 'react';
+// const JobItem = ({ job, onDelete }) => {
+//   const { id, title, description, company, location, date_posted } = job;
 
-// const JobItem = ({ job }) => {
+//   const handleDeleteClick = () => {
+//     onDelete(id);
+//   };
+
 //   return (
-//     <li className="job-detail">
-//       <h2>{job.title}</h2>
-//       <p>Description: {job.description}</p>
-//       <p>Location: {job.location}</p>
-//       {/* Add more details as needed */}
-//     </li>
+//     <div className="card">
+//       <h3>{title}</h3>
+//       <p className="company">{company}</p>
+//       <p className='description'>{description}</p>
+//       <div className="extra content">
+//           <span>
+//           <i class="fa-solid fa-location-dot"></i>
+//             {location}
+//           </span>
+//           <span>
+//             <i className="fa-solid fa-calendar-days"></i>
+//             { date_posted}
+//           </span>
+//           <span className="delete-icon-container" onClick={handleDeleteClick}>
+//             <i className="fa-solid fa-pen"></i>
+//             <span className="delete-text">Edit</span>
+//           </span>
+//           <span className="delete-icon-container" onClick={handleDeleteClick}>
+//             <i className="fa-solid fa-trash-can"></i>
+//             <span className="delete-text">Delete</span>
+//           </span>
+//         </div>
+
+      
+//     </div>
 //   );
-// }
+// };
 
 // export default JobItem;
 
 
-import React from 'react';
-//import './styles.css'; // Import your CSS file
+import React, { useState } from 'react';
 
-const JobItem = ({ job }) => {
-    const { title, description, company, location, date_posted } = job;
 
-    return (
-        <div className="card">
-            <h3>{title}</h3>
-            <p className="company">{company}</p>
-            <p className="location">{location}</p>
-            <p>{description}</p>
-            <p className="posted">Posted on: {date_posted}</p>
-        </div>
-    );
+const JobItem = ({ job, onDelete, onEdit }) => {
+  const { id, title: initialTitle, description: initialDescription, company: initialCompany, location: initialLocation, date_posted: initialDatePosted } = job;
+  
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(initialTitle);
+  const [description, setDescription] = useState(initialDescription);
+  const [company, setCompany] = useState(initialCompany);
+  const [location, setLocation] = useState(initialLocation);
+  const [datePosted, setDatePosted] = useState(initialDatePosted);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleDeleteClick = () => {
+        onDelete(id);
+      };
+    
+
+  const handleSaveClick = () => {
+    const editedJob = {
+      id,
+      title,
+      description,
+      company,
+      location,
+      date_posted: datePosted
+    };
+    onEdit(editedJob);
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    // Reset the input fields to initial values
+    setTitle(initialTitle);
+    setDescription(initialDescription);
+    setCompany(initialCompany);
+    setLocation(initialLocation);
+    setDatePosted(initialDatePosted);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="card">
+      {isEditing ? (
+        <>
+          {/* <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <textarea value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+          <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
+          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+          <input type="date" value={datePosted} onChange={(e) => setDatePosted(e.target.value)} />
+          <div>
+          <span className="delete-icon-container" onClick={handleSaveClick}>
+             <i className="fa-solid fa-pen"></i>
+             <span className="delete-text">Save</span>
+           </span>
+           <span className="delete-icon-container" onClick={handleCancelClick}>
+             <i className="fa-solid fa-trash-can"></i>
+             <span className="delete-text">Cancel</span>
+           </span>
+          </div> */}
+          <form  className="job-form edit">
+            <label>
+                Title:
+                <input type="text" name="title" value={title}   onChange={(e) => setTitle(e.target.value)}  required />
+            </label>
+            <label>
+                Description:
+                <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} required />
+            </label>
+            <label>
+                Company:
+                <input type="text" name="company" value={company}  onChange={(e) => setCompany(e.target.value)} required />
+            </label>
+            <label>
+                Location:
+                <input type="text" name="location" value={location} onChange={(e) => setLocation(e.target.value)} required />
+            </label>
+            <label>
+                Date Posted:
+                <input type="date" name="date_posted" value={datePosted} onChange={(e) => setDatePosted(e.target.value)}required />
+            </label>
+            <div className='button-holder'>
+              <button type="button" onClick={handleSaveClick}>Save</button>
+              <button type="button" onClick={handleCancelClick}>Cancel</button>
+            </div>
+        </form>
+        </>
+      ) : (
+        <>
+        <h3>{title}</h3>
+        <p className="company">{company}</p>
+        <p className='description'>{description}</p>
+        <div className="extra content">
+            <span>
+            <i class="fa-solid fa-location-dot"></i>
+              {location}
+            </span>
+            <span>
+              <i className="fa-solid fa-calendar-days"></i>
+              {datePosted}
+            </span>
+            <span className="delete-icon-container" onClick={handleEditClick}>
+              <i className="fa-solid fa-pen"></i>
+              <span className="delete-text">Edit</span>
+            </span>
+            <span className="delete-icon-container" onClick={handleDeleteClick}>
+              <i className="fa-solid fa-trash-can"></i>
+              <span className="delete-text">Delete</span>
+            </span>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default JobItem;
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import JobItem from './JobItem';
 
-
 const JobList = () => {
   const [jobs, setJobs] = useState([]);
 
@@ -10,19 +9,44 @@ const JobList = () => {
     axios.get('http://localhost:4000/jobs')
       .then(response => {
         setJobs(response.data);
-        console.log("Uzee");
       })
       .catch(error => {
         console.error('Error fetching jobs:', error);
       });
   }, []);
 
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:4000/jobs/${id}`)
+      .then(response => {
+        setJobs(prevJobs => prevJobs.filter(job => job.id !== id));
+        console.log('Job deleted successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error deleting job:', error);
+      });
+  };
+
+  const handleEdit = (editedJob) => {
+    axios.put(`http://localhost:4000/jobs/${editedJob.id}`, editedJob)
+      .then(response => {
+        setJobs(prevJobs => prevJobs.map(job => {
+          if (job.id === editedJob.id) {
+            return editedJob;
+          }
+          return job;
+        }));
+        console.log('Job updated successfully:', response.data);
+      })
+      .catch(error => {
+        console.error('Error updating job:', error);
+      });
+  };
+
   return (
     <div className="job-list">
-      {/* <h2>Job Listings</h2> */}
       <ul className='container'>
         {jobs.map(job => (
-          <JobItem key={job.id} job={job} />
+          <JobItem key={job.id} job={job} onDelete={handleDelete} onEdit={handleEdit} />
         ))}
       </ul>
     </div>
