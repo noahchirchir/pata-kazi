7dc4617b3c9be82324b5be0f76fe0d09c2a199c7
Chrispine Oburu <chrizoburu65@gmail.com>
2024-05-10 11:21:18 +0300

    add filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,5 @@
-// import React from 'react';
-// import NavBar from "./NavBar";
-// const Home = () => {
-
-//   return (
-//     <>
-//         <header>
-//         <NavBar />
-//         </header>
-//         <div className="home">
-//         <h2>Welcome to our Job Listing App!</h2>
-//         <p>Find talented individuals or discover job opportunities.</p>
-//         </div>
-//     </>
-    
-//   );
-// }
-
-// export default Home;
-
 import React from 'react';
 import NavBar from "./NavBar";
-// import images from 'src/components/backgroundimage.jpg'
 
 
 const Home = () => {
@@ -28,19 +7,19 @@ const Home = () => {
 
   return (
     <>
-        <header>
+      <header>
         <NavBar />
-        </header>
-        <section className='hero'>
+      </header>
+      <section className='hero'>
         <div className="home">
           <center>
-        <h1>Welcome to our Job Listing App!</h1>
-        <p>Find talented individuals or discover job opportunities.</p>
-        </center>
+            <h1>Welcome to our Job Listing App!</h1>
+            <p>Find talented individuals or discover job opportunities.</p>
+          </center>
         </div>
-        </section>
+      </section>
     </>
-    
+
   );
 }
 
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,55 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import JobItem from './JobItem';
 
-const JobList = () => {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://job-lists.onrender.com/jobs')
-      .then(response => {
-        setJobs(response.data);
-        console.log('Jobs fetched successfully');
-      })
-      .catch(error => {
-        console.error('Error fetching jobs:', error);
-      });
-  }, []);
-
-  const handleDelete = (id) => {
-    axios.delete(`https://job-lists.onrender.com/jobs/${id}`)
-      .then(response => {
-        setJobs(prevJobs => prevJobs.filter(job => job.id !== id));
-        console.log('Job deleted successfully:', response.data);
-      })
-      .catch(error => {
-        console.error('Error deleting job:', error);
-      });
-  };
-
-  const handleEdit = (editedJob) => {
-    axios.put(`https://job-lists.onrender.com/jobs/${editedJob.id}`, editedJob)
-      .then(response => {
-        setJobs(prevJobs => prevJobs.map(job => {
-          if (job.id === editedJob.id) {
-            return editedJob;
-          }
-          return job;
-        }));
-        console.log('Job updated successfully:', response.data);
-        window.location.reload();
-      })
-      .catch(error => {
-        console.error('Error updating job:', error);
-      });
-    
-  };
-
+function JobList({filteredJobs}){
   return (
     <div className="job-list">
       <ul className='container'>
-        {jobs.map(job => (
-          <JobItem key={job.id} job={job} onDelete={handleDelete} onEdit={handleEdit} />
+        {filteredJobs.map(job => (
+          <JobItem key={job.id} job={job} />
         ))}
       </ul>
     </div>
@@ -57,4 +14,3 @@ const JobList = () => {
 }
 
 export default JobList;
-
